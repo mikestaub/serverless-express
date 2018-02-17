417de081cee87c84b8f12fca7fcd1b301c2c02cb
iliasbhal <ilias.bhal@gmail.com>
2018-02-17 18:42:42 -0500

    removed small dead code

--- test/src/init-plugin.js
+++ test/src/init-plugin.js
@@ -10,7 +10,7 @@ module.exports = function(o){
   
     serverless.variables.service.provider.name = options.provider 
     if( options.noEnv == true  ){ delete serverless.variables.service.provider.environment }
-    if( options.noInit == true ){ console.log('noInit');sls_express.prototype._initialize = ()=>{} }
+    if( options.noInit == true ){ sls_express.prototype._initialize = ()=>{} }
 
 
     return new sls_express(serverless, plugin_options)
